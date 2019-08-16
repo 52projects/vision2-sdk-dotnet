2f2eddf23aabc1a1720ab350154c9162f70b6879
Chad Meyer <chadmeyer@52projectsllc.com>
2019-08-15 22:13:37 -0500

    creating a way to search for payments in v2

---
+++ src/lib/QueryObject/PaymentQO.cs
@@ -0,0 +1,10 @@
+﻿using Vision2.Api.Attributes;
+using Vision2.Api.Enum;
+
+
+namespace Vision2.Api.QueryObject {
+    public class PaymentQO : BaseQO {
+        [QO("PackageID")]
+        public int? PackageID { get; set; }
+    }
+}
--- src/lib/Sets/PaymentSet.cs
+++ src/lib/Sets/PaymentSet.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 namespace Vision2.Api.Sets {
     public class PaymentSet : BaseApiSet<Payment> {
         private const string _getUrl = "/payment/{0}";
+        private const string _searchUrl = "/search/appliedpaymentdetails";
 
         public PaymentSet(Vision2Token token, string apiUrl) : base(token, apiUrl) {
 
@@ -13,8 +14,14 @@ namespace Vision2.Api.Sets {
         
         protected override string GetUrl => _getUrl;
 
+        protected override string SearchUrl => _searchUrl;
+
         public IVision2RestResponse<Vision2Response<PaymentAdjustment>> MakePaymentAdjustments(PaymentAdjustment adjustments) {
             return Create(adjustments, "/payment/adjust");
+        }
+
+        public IVision2RestResponse<Vision2PagedResponse<Payment>> Find(PaymentQO qo) {
+            return Search<Payment>(qo);
         }
     }
 }
